d3e3e098508101cd6273dcf9da104cfc65f22d3f
dlabaja <dlabaja.jan@seznam.cz>
2026-03-29 17:03:28 +0200

    lsitener controls

--- SharpLisp/Listener/Listener.cs
+++ SharpLisp/Listener/Listener.cs
@@ -4,17 +4,12 @@ public static class Listener
 {
     public static void Run()
     {
+        new ListenerControls().Run();
         Console.WriteLine("-------------------");
         Console.WriteLine("SharpLisp listerner");
         Console.WriteLine("-------------------\n");
         Console.WriteLine("Type '#HELP' for help.\n");
 
-        while (true)
-        {
-            Console.Write("> ");
-            var input = Console.ReadLine();
-            ListenerCommands.ResolveCommand(input?.Trim() ?? "");
-            Console.WriteLine();
-        }
+        Console.Write("> ");
     }
 }
